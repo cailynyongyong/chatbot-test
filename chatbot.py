9c3f9ca31b62767ebacf99985147af59a6a3980a
Cailyn Yong <cailynyongyong@gmail.com>
2023-12-04 15:55:30 +0900

    fix

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
